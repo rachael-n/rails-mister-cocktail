118637ddb176cbbb1fd73dc3841bdc57cc927b68
Rachael Neumann <rachael.neumann@me.com>
2020-02-27 14:39:20 +0800

    created routed for cocktails and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cocktails', to: 'cocktails#index', as: 'cocktails'
+  get 'cocktails/new', to: 'cocktails#new', as: 'new_cocktail'
+  get 'cocktails/:id', to: 'cocktails#show', as: 'cocktail'
+  post 'cocktails', to: 'cocktails#create'
+
+  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: 'new_dose'
+  post 'cocktails/:cocktail_id/doses', to: 'doses#create'
+  delete 'doses/:id', to: 'doses#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'faker'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drink_data = JSON.parse(open(url).read)
+
+faker_ingredients = []
+drink_data['drinks'].each do |item|
+  faker_ingredients << item['strIngredient1']
+end
+
+puts 'Clearing out the cocktail list.'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Making a fresh batch.'
+
+15.times do
+  faker_cocktail = Cocktail.new(name: Faker::Dessert.unique.flavor)
+  faker_cocktail.save
+
+  3.times do
+    faker_ingredient = Ingredient.new(name: faker_ingredients.sample)
+    faker_ingredient.save
+
+    faker_dose = Dose.new(
+      description: Faker::Food.measurement,
+      ingredient_id: faker_ingredient.id,
+      cocktail_id: faker_cocktail.id
+    )
+    faker_dose.save
+  end
+  faker_cocktail.save!
+end
